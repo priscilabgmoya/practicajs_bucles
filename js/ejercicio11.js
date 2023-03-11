1f582d32a1935eff662e000d74d28c485e7d2c3c
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-03-10 21:21:30 -0300

    se agrego el ejercicio 11 del tp de bucles correccion de formato de pregunta de las edad mayor

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -5,26 +5,35 @@ Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp
 
 edadMayor();
 
-function edadMayor(){
-    let edad1 =parseInt(prompt('ingrese la primera edad'));
-    let nombre1 = prompt('ingrese el primer nombre'); 
-    let edad2 =parseInt(prompt('ingrese la segunda edad'));
-    let nombre2 = prompt('ingrese el segundo nombre'); 
-    let edad3 =parseInt(prompt('ingrese la tercero edad'));
-    let nombre3 = prompt('ingrese el tercer nombre'); 
+function edadMayor() {
+  let edad1 = parseInt(prompt("ingrese la primera edad"));
+  let nombre1 = prompt("ingrese el primer nombre");
+  let edad2 = parseInt(prompt("ingrese la segunda edad"));
+  let nombre2 = prompt("ingrese el segundo nombre");
+  let edad3 = parseInt(prompt("ingrese la tercero edad"));
+  let nombre3 = prompt("ingrese el tercer nombre");
 
-    if(! isNaN(edad1) &&! isNaN(edad2) && ! isNaN(edad3)  ){
-        let resultado = Math.max(edad1,edad2,edad3); 
-        if(resultado === edad1){
-            document.write(`<p style= "color: black; font-size: 30px">El nombre del mayor es:  <span style= "color: red; font-size: 30px" >${nombre1}</span></p>`)
-        }
-        if(resultado === edad2){
-            document.write(`<p style= "color: black; font-size: 30px">El nombre del mayor es:  <span style= "color: red; font-size: 30px" >${nombre2}</span></p>`)
-        }
-        if(resultado === edad3){
-            document.write(`<p style= "color: black; font-size: 30px">El nombre del mayor es: <span style= "color: red; font-size: 30px" >${nombre3}</span></p>`)
-        }
-    }else{
-        alert('ingrese una edad valida');
+  if (!isNaN(edad1) && !isNaN(edad2) && !isNaN(edad3)) {
+    let resultado = Math.max(edad1, edad2, edad3);
+    switch (resultado) {
+      case edad1:
+        document.write(
+          `<p style= "color: black; font-size: 30px">El nombre del mayor es:  <span style= "color: red; font-size: 30px" >${nombre1}</span></p>`
+        );
+        break;
+      case edad2:
+        document.write(
+          `<p style= "color: black; font-size: 30px">El nombre del mayor es:  <span style= "color: red; font-size: 30px" >${nombre2}</span></p>`
+        );
+        break;
+
+      default:
+        document.write(
+          `<p style= "color: black; font-size: 30px">El nombre del mayor es: <span style= "color: red; font-size: 30px" >${nombre3}</span></p>`
+        );
+        break;
     }
+  } else {
+    alert("ingrese una edad valida");
+  }
 }
